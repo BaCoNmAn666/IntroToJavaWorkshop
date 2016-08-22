70188b1e3f11663ce2cc90a59dd616ff3474b24c
BaCoNmAn666 <league@Mac-mini-2.local>
2016-08-22 12:07:31 -0700

    DRAWING
    we drew pentagons with robots named timmy, james, esme and apple

---
+++ src/day1/robot/BetterSquare.java
@@ -0,0 +1,33 @@
+package day1.robot;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class BetterSquare {
+     public static void main(String[] args) {
+		
+    	 Robot Esme = new Robot("r2d2");
+    	//1. Move the robot		
+    
+    	Esme.penDown();
+    	Esme.setPenColor(0, 0, 255);
+    	Esme.setSpeed(10);
+    	for (int i = 0; i < 4; i++) {
+			Esme.setRandomPenColor();
+    		Esme.move(500);
+			Esme.turn(270);
+		}
+    	Esme.move(250);
+    	Esme.turn(270);
+    	Esme.move(500);
+    	Esme.turn(90);
+    	Esme.move(250);
+    	Esme.turn(90);
+    	Esme.move(250);
+    	Esme.turn(90);
+    	Esme.move(500);
+    	Esme.turn(90);
+    	Esme.move(250);
+    	
+    	
+	}
+}
--- src/day1/robot/MyFirstJavaProgram.java
+++ src/day1/robot/MyFirstJavaProgram.java
@@ -7,9 +7,36 @@ public class MyFirstJavaProgram {
 	public static void main(String[] args) {
 		
 		// START HERE
-
+		
 	
 		
-		
+		Robot Timmy = new Robot();
+	
+	Timmy.setSpeed(10);
+	Timmy.clear();
+	Timmy.getX();
+	Timmy.penDown();
+	Timmy.setPenColor(0, 0, 255);
+	Timmy.getX();
+	Timmy.getY();
+	Timmy.move(500);
+	Timmy.turn(270);
+	Timmy.move(500);
+	Timmy.turn(270);
+	Timmy.move(500);
+	Timmy.turn(270);
+	Timmy.move(500);
+	Timmy.turn(270);
+	Timmy.move(250);
+	Timmy.turn(270);
+	Timmy.move(500);
+	Timmy.turn(90);
+	Timmy.move(250);
+	Timmy.turn(90);
+	Timmy.move(250);
+	Timmy.turn(90);
+	Timmy.move(500);
+	
+	
 	}
 }
